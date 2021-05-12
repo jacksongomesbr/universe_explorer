f9256da32615a7b8cd151fdc8da4c856c59984a9
Jackson Gomes <jgomes@ceulp.edu.br>
2021-05-12 01:25:05 -0300

    Adiciona o método estático que constrói uma instância da classe a partir de um Map (representa um json)

--- lib/universe_data.dart
+++ lib/universe_data.dart
@@ -1,10 +1,31 @@
+/// A estrutura da classe de dados utilizada no aplicativo para
+/// armazenar/representar dados de um planeta.
 class Planet {
+  /// O identificador do planeta.
   final String id;
+
+  /// O caminho da imagem do planeta (na pasta `assets`)
   final String image;
+
+  /// O nome do planeta.
   final String name;
+
+  /// A descrição do planeta.
   final String description;
+
+  /// A indicação de o planeta ter sido curtido (true) ou não (false).
   bool like;
 
   Planet(this.id, this.image, this.name, this.description, this.like);
-}
 
+  /// Constrói uma instância de [Planet].
+  ///
+  /// É fornecido como um método estático e o [json] informado como
+  /// parâmetro contém um [Map] com as informações do planeta.
+  Planet.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        image = json['image'],
+        name = json['name'],
+        description = json['description'],
+        like = json['like'];
+}
